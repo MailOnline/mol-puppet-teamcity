5796697e88b3ebbcc9b8b2ed31b54bd9f6ba28fa
Miguel Arranz <miguel.arranz@dmgmedia.tech>
2023-12-01 10:44:47 +0000

    first commit

---
+++ manifests/agent.pp
@@ -0,0 +1,34 @@
+class teamcity::agent(
+  $agent_name = $title,
+  $master_url = undef,
+  $port       = '9090',)
+{
+  if $master_url == undef {
+    fail("Teamcity::Agent[${agent_name}]: Please set \$master_url")
+  }
+  $download_url = "http://teamcity.localdomain:8111/update/buildAgent.zip"
+  $download_path = "/tmp/teamcity.zip"
+  $destination_path = "/opt/teamcity-agent"
+
+  $packages = ['wget','unzip']
+  package { $packages:
+    ensure => present,
+  }
+
+  Archive {
+    provider => 'wget',
+    require => Package['wget']
+  }
+  file {$destination_path:
+    ensure => 'directory'
+  }
+
+  archive { $download_path:
+    ensure => present,
+    source => $download_url,
+    extract         => true,
+    extract_path    => $destination_path,
+    cleanup         => true,
+  }
+
+}
