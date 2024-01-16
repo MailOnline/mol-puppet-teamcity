e5025b31be4a3a728e44b58b91722631a6218b5e
Miguel Arranz <miguel.arranz@dmgmedia.tech>
2024-01-16 10:44:56 +0000

    adding systemd config

---
+++ manifests/systemd.pp
@@ -0,0 +1,34 @@
+class teamcity::agent::service::systemd(
+  $agent_name = $title,
+  ) {
+
+  $agent_dir = "/opt/teamcity_agent_${agent_name}/"
+
+  file { '/lib/systemd/system/build-agent.service':
+    ensure  => 'file',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    content => template("${module_name}/build-agent-service.erb"),
+    before  => Service['build-agent.service'],
+    notify  => Exec['systemd_reload'],
+  }
+
+  exec { 'systemd_reload':
+    command     => '/bin/systemctl daemon-reload',
+    refreshonly => true,
+  }
+
+  service { 'build-agent.service':
+    ensure     => 'running',
+    enable     => 'true',
+    hasstatus  => true,
+    hasrestart => true,
+    provider   => 'systemd',
+    require    => Exec['systemd_reload'],
+  }
+
+  file { '/etc/init.d/build-agent':
+    ensure  => absent,
+  }
+}
